0858f2b1155d71faf3e1966dc8b57c67b46beb66
aymiller <aymill@gmail.com>
2014-12-08 17:37:53 -0800

    Init

---
+++ DblSubmitBurpExt/src/burp/BurpExtender.java
@@ -0,0 +1,9 @@
+package main;
+
+public class BurpExtender implements IBurpExtender{
+	public void registerExtenderCallbacks(
+	        IBurpExtenderCallbacks callbacks)
+	    {
+	        // your extension code here
+	    }
+}
